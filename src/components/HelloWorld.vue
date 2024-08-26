90b1466813355b9d8d7326ff9cb10232eb5b523b
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-08-26 15:22:46 +0200

    feat: basic html, css & js-Structure implemented

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,93 @@
-<script setup>
-import { ref } from "vue";
-
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
+<script>
+export default {
+  data() {
+    return {
+      benutzerName: "",
+      produktionsNummer: "",
+      produkt: "",
+      dieStückzahl: "",
+    };
+  },
+  props: {
+    msg: String,
+  },
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div-whole-area>
+    <h1>{{ msg }}</h1>
+    <div class="input-area">
+      <div class="before-programm-starts">
+        <input type="text" placeholder="Benutzer" v-model.trim="benutzerName" />
+        <input
+          type="text"
+          placeholder="Produktions-Nummer"
+          v-model.trim="produktionsNummer"
+        />
+        <input type="text" placeholder="Produkt" v-model.trim="produkt" />
+      </div>
+      <input type="text" placeholder="Stückzahl" v-model.trim="dieStückzahl" />
+    </div>
+    <div class="button-area">
+      <button type="button">▶</button>
+      <button type="button">||</button>
+      <button type="button">■</button>
+      <button type="button">Senden</button>
+    </div>
+    <div class="time-area">
+      <p>
+        <span>00</span><span class="colon">:</span><span>00</span
+        ><span class="colon">:</span><span>00</span>
+      </p>
+    </div>
+  </div-whole-area>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.input-area {
+  display: flex;
+  gap: 3rem;
+}
+
+input {
+  font-size: 1.5rem;
+}
+
+.before-programm-starts {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.input-area > input {
+  height: fit-content;
+}
+
+.button-area {
+  margin-top: 3rem;
+}
+
+button {
+  font-size: 2rem;
+  border: 0.2rem solid black;
+}
+
+button + button {
+  margin-left: 1rem;
+}
+
+p {
+  font-size: 3rem;
+  position: relative;
+}
+
+span {
+  margin-right: 0.25rem;
+}
+
+.colon {
+  position: relative;
+  top: -3px;
 }
 </style>
